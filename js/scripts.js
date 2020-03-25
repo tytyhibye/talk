13fad83eb95a7b629026599ab7620113cc98c6e9
Tyler Bates <Fondlytyler@gmail.com>
2020-03-25 09:34:14 -0700

    buttons funtion to respond and remove responses.

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,21 @@ $(document).ready(function() {
   $("button#hello").click(function() {
     $("ul#user").prepend("<li>Hello!</li>");
     $("ul#webpage").prepend("<li>Why hello there!</li>");
-    $('li').css('background-color', 'grey');
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    }); 
+      $("ul#webpage").children("li").first().click(function() {
+        $(this).remove();
+    });
   });
 
   $("button#goodbye").click(function() {
     $("ul#user").prepend("<li>Goodbye!</li>");
-    $("ul#webpage").prepend("<li>Goodbye, dear user!</li>");
-    $('li').css('background-color', 'yellow');
+    $("ul#webpage").prepend("<li>Goodbye, dear user.</li>");
   });
 
   $("button#stop").click(function() {
     $("ul#user").prepend("<li>Stop copying me!</li>");
-    $("ul#webpage").prepend("<li>Pardon me. I meant no offense.</li>");
-    $('li').css('background-color', 'green');
+    $("ul#webpage").prepend("<li>Pardon me, I meant big time offense.</li>");
   });
 });
